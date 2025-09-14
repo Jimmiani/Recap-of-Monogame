9eea9af60dffd56e67f6756210a0938a75a84d08
Emmett Cornies <339761330@GoTVDSB.ca>
2025-09-14 02:57:29 -0400

    font not working

--- Recap of Monogame/Game1.cs
+++ Recap of Monogame/Game1.cs
@@ -9,15 +9,26 @@ namespace Recap_of_Monogame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        SpriteFont gameFont;
+
         Rectangle window;
+        Texture2D backgroundTexture;
 
         Texture2D radianceTexture;
         Rectangle radianceRect;
+
         Texture2D sunDialTexture;
         Rectangle sunDialRect;
+        float dialRotation;
 
         Texture2D knightTexture;
         Rectangle knightRect;
+
+        Texture2D platformTexture;
+        Rectangle platformRect;
+
+        Texture2D cloudTexture;
+        Rectangle cloudRect;
 
         public Game1()
         {
@@ -33,12 +44,16 @@ namespace Recap_of_Monogame
             _graphics.PreferredBackBufferHeight = 720;
             _graphics.ApplyChanges();
 
-
             window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            dialRotation = 0;
 
             base.Initialize();
 
-            radianceRect = new Rectangle(window.Width / 2 - radianceTexture.Width / 4, 100, radianceTexture.Width / 2, radianceTexture.Height / 2);
+            radianceRect = new Rectangle(window.Width / 2 - 275, 50, 551, 600);
+            sunDialRect = new Rectangle(645, 300, 1300, 1300);
+            cloudRect = new Rectangle(100, 400, 1200, 700);
+            platformRect = new Rectangle(600, 500, platformTexture.Width, platformTexture.Height);
+            knightRect = new Rectangle(630, 360, knightTexture.Width, knightTexture.Height);
         }
 
         protected override void LoadContent()
@@ -48,6 +63,11 @@ namespace Recap_of_Monogame
             knightTexture = Content.Load<Texture2D>("Images/Knight/Challenge Start_011");
             radianceTexture = Content.Load<Texture2D>("Images/Radiance/Cast_003");
             sunDialTexture = Content.Load<Texture2D>("Images/Radiance/Sun Dial");
+            backgroundTexture = Content.Load<Texture2D>("Images/Background/hollow_knight_background");
+            cloudTexture = Content.Load<Texture2D>("Images/Clouds/blurry_cloud");
+            platformTexture = Content.Load<Texture2D>("Images/Platforms/R Plat Wide Idle_000");
+            knightTexture = Content.Load<Texture2D>("Images/Knight/Challenge Start_011");
+            gameFont = Content.Load<SpriteFont>("gameFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -55,7 +75,7 @@ namespace Recap_of_Monogame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            dialRotation += 0.005f;
 
             base.Update(gameTime);
         }
@@ -66,8 +86,13 @@ namespace Recap_of_Monogame
 
             _spriteBatch.Begin();
 
+            _spriteBatch.Draw(backgroundTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(sunDialTexture, sunDialRect, null, Color.White, dialRotation, new Vector2(sunDialTexture.Width / 2, sunDialTexture.Height / 2), SpriteEffects.None, 0);
             _spriteBatch.Draw(radianceTexture, radianceRect, Color.White);
-
+            _spriteBatch.Draw(knightTexture, knightRect, null, Color.White, 0f, Vector2.Zero, SpriteEffects.FlipHorizontally, 0f);
+            _spriteBatch.Draw(platformTexture, platformRect, Color.White);
+            _spriteBatch.Draw(cloudTexture, cloudRect, Color.White * 0.9f);
+            _spriteBatch.DrawString(gameFont, new string("Hollow Knight"), new Vector2(100, 550), Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
